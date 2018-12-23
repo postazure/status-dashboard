ed3b306f97ebf77d31557b7f4efbdcbd83f08827
Max Gordon <gordonmaxc@gmail.com>
2018-12-22 16:55:40 -0800

    Events are displayed on the feed, served via api

--- app/javascript/packs/components/Feed.js
+++ app/javascript/packs/components/Feed.js
@@ -1,9 +1,20 @@
 import React from "react"
 
 export default class Feed extends React.Component {
+  state = {events: []}
+  
+  componentDidMount() {
+    fetch("/api/events")
+      .then(res => res.json())
+      .then(json => this.setState({events: json.events}))
+  }
+  
 	render() {
 		return(
-			<div>Feed</div>
+      <div>
+	  		<div>Feed</div>
+        {this.state.events.map(event => <div key={event.id}>{event.description}</div>)}
+      </div>
 		)
 	}
 }
